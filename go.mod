cff65bfcd3a8f16b7656ec89e31cec215c2dda31
Seanbon0611 <sean.dever@datadoghq.com>
2021-12-08 17:57:35 -0800

    created requested to reqtweets for a post on twitter, but as of right now is returning 401 Unauthorized. This works when cURLing or on Postman though...

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module Seanbon0611/retweet_picker
 
 go 1.17
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require github.com/joho/godotenv v1.4.0
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -13,10 +15,28 @@ func main() {
 	if err != nil {
 		log.Fatal("Error Loading .env file")
 	}
-	ApiKey := os.Getenv("API_KEY")
-	ApiSecret := os.Getenv("API_KEY_SECRET")
+	// ApiKey := os.Getenv("API_KEY")
+	// ApiSecret := os.Getenv("API_KEY_SECRET")
+	BearerToken := "Bearer" + " " + os.Getenv("BEARER_TOKEN")
 
-	fmt.Println(ApiKey)
-	fmt.Println(ApiSecret)
+	req := getRetweeters("https://api.twitter.com/2/tweets/1341196353582419969/retweeted_by", BearerToken)
+	defer req.Body.Close()
 
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(BearerToken)
+	fmt.Println(string(body))
+
+}
+
+func getRetweeters(endpoint string, token string) *http.Response {
+	req, err := http.Get(endpoint)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("authorization", token)
+	req.Header.Add("Content-Type", "application/json; charset=utf-8")
+	return req
 }
