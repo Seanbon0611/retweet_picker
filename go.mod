5907397a7edfa6005cd610767de335b4633cf4af
Seanbon0611 <sean.dever@datadoghq.com>
2022-01-04 22:12:13 -0800

    setup oauth2 to get bearer token and added functionality to make request to get all users who have retweeted a tweet

--- go.mod
+++ go.mod
@@ -3,3 +3,11 @@ module Seanbon0611/retweet_picker
 go 1.17
 
 require github.com/joho/godotenv v1.4.0
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,42 +1,64 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
 )
 
 func main() {
+	//loads env file that has key and secret
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error Loading .env file")
 	}
-	// ApiKey := os.Getenv("API_KEY")
-	// ApiSecret := os.Getenv("API_KEY_SECRET")
-	BearerToken := "Bearer" + " " + os.Getenv("BEARER_TOKEN")
+	ApiKey := os.Getenv("API_KEY")
+	ApiSecret := os.Getenv("API_KEY_SECRET")
 
-	req := getRetweeters("https://api.twitter.com/2/tweets/1341196353582419969/retweeted_by", BearerToken)
-	defer req.Body.Close()
+	//function that makes an auth request to retieve vearer token
+	tokenReq := getToken("https://api.twitter.com/oauth2/token?grant_type=client_credentials", ApiKey, ApiSecret)
 
-	body, err := ioutil.ReadAll(req.Body)
+	var client http.Client
+	tokenRes, err := client.Do(tokenReq)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(BearerToken)
-	fmt.Println(string(body))
+	defer tokenRes.Body.Close()
+
+	var token oauth2.Token
+	dec := json.NewDecoder(tokenRes.Body)
+	err = dec.Decode(&token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	conf := &oauth2.Config{}
+
+	twitterClient := conf.Client(ctx, &token)
+	twitterRes, err := twitterClient.Get("https://api.twitter.com/2/tweets/1341196353582419969/retweeted_by")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer twitterRes.Body.Close()
+	//outputs response body within the terminal of all users who retweeted the tweet associated with the tweet_id
+	io.Copy(os.Stdout, twitterRes.Body)
 
 }
 
-func getRetweeters(endpoint string, token string) *http.Response {
-	req, err := http.Get(endpoint)
+func getToken(endpoint string, key string, secret string) *http.Request {
+	req, err := http.NewRequest("POST", endpoint, strings.NewReader(""))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	req.Header.Add("authorization", token)
-	req.Header.Add("Content-Type", "application/json; charset=utf-8")
+	req.SetBasicAuth(key, secret)
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 	return req
 }
